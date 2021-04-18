42e887c0dedfb185486381352bca061a799c73af
Dimitar Atanasov <dimitar.atanasov@adon.solutions>
2021-04-19 00:08:58 +0100

    Updating user-data to display env

--- services/web_cluster/user-data.sh
+++ services/web_cluster/user-data.sh
@@ -1,7 +1,11 @@
 #!bin/bash
 
+os_release = $(lsb_release -a)
+
 cat > /home/ubuntu/index.html <<EOF
-<h1>Hello, World!</h1>
+<h1>Hello, Ubuntu!</h1>
+<p> Env: ${os_release}</p>
+<br>
 <p>DB Address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
