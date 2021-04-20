99588ab1e123d208dda1f0f132b24ec4d31b5196
Dimitar Atanasov <dimitar.atanasov@adon.solutions>
2021-04-21 00:46:51 +0100

    Adding subnet_ids var to alb module

--- networking/alb/main.tf
+++ networking/alb/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lb" "initial_alb" {
   name = var.alb_name
   load_balancer_type = "application"
-  subnets = data.aws_subnet_ids.default.ids
+  subnets = var.subnet_ids
   security_groups = [aws_security_group.alb_sg.id]
 }
 
